93ccc7b4ad00de8d7809a85c180091a815461c4a
skkim125 <kthanks125@gmail.com>
2025-06-13 21:16:20 +0900

    [Feat] NetworkManager 생성 & TMDBTarget 추가
    - RxSwift의 Single 기반 NetworkManager 생성
    - TMDBTarget의 TV 케이스에 대한 설정
    - NetworkError 요소 추가

--- RoomReviewer/RoomReviewer/Network/NetworkError.swift
+++ RoomReviewer/RoomReviewer/Network/NetworkError.swift
@@ -8,5 +8,10 @@
 import Foundation
 
 enum NetworkError: Error {
+    case invalidRequest
     case invalidURL
+    case invalidResponse
+    case invalidData
+    case decodingError
+    case commonError
 }
