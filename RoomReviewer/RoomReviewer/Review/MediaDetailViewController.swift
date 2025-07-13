4d5694f5bafbf6a5e469e21b14578e50b7929dbe
skkim125 <kthanks125@gmail.com>
2025-07-13 14:47:41 +0900

    [Feat] 미디어 디테일뷰 생성 및 로직 설정 - MediaDetailViewController 임시 생성 - 검색 결과 컬렉션뷰에서 미디어 선택 시 MediaDetailViewController로 push되도록 설정

---
+++ RoomReviewer/RoomReviewer/Review/MediaDetailViewController.swift
@@ -0,0 +1,22 @@
+//
+//  MediaDetailViewController.swift
+//  RoomReviewer
+//
+//  Created by 김상규 on 7/13/25.
+//
+
+import UIKit
+import RxSwift
+import RxCocoa
+import SnapKit
+import Then
+
+final class MediaDetailViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .white
+    }
+    
+}
