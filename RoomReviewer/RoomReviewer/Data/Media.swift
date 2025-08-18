20c418dd56bb63c65c813c1a2ad242adf480da5c
skkim125 <kthanks125@gmail.com>
2025-08-18 23:38:56 +0900

    [Feat] 미디어 상세뷰에서 영화 정보 표시 구현(1) - 미디어 상세뷰에서 영화정보 표시 로직 구현 - Media 데이터에 영화는 개봉일자, 드라마는 첫 방영일자를 가지는 releaseDate 프로퍼티 추가 - 미디어 제목 레이블에 제목과 연도를 구분한 디자인으로 표시하는 메서드 setTitleStyle 추가

--- RoomReviewer/RoomReviewer/Data/Media.swift
+++ RoomReviewer/RoomReviewer/Data/Media.swift
@@ -15,6 +15,7 @@ struct Media: Equatable {
     let posterPath: String?
     let backdropPath: String?
     let genreIDS: [Int]
+    let releaseDate: String?
 }
 
 enum MediaType: String, Codable {
@@ -25,10 +26,10 @@ enum MediaType: String, Codable {
 
 extension Media {
     static var mockTV: Media {
-        Media(id: 261980, mediaType: .tv, title: "미지의 서울", overview: "얼굴 빼고 모든 게 다른 쌍둥이 자매가 인생을 맞바꾸는 거짓말로 진짜 사랑과 인생을 찾아가는 로맨틱 성장 드라마", posterPath: "/m0VuPoWQhbgMjVIwAdZmmHgHQrl.jpg", backdropPath: "/woGYRE5vChxqUqTBJJaOhO9Cqk6.jpg",  genreIDS: [35, 18])
+        Media(id: 261980, mediaType: .tv, title: "미지의 서울", overview: "얼굴 빼고 모든 게 다른 쌍둥이 자매가 인생을 맞바꾸는 거짓말로 진짜 사랑과 인생을 찾아가는 로맨틱 성장 드라마", posterPath: "/m0VuPoWQhbgMjVIwAdZmmHgHQrl.jpg", backdropPath: "/woGYRE5vChxqUqTBJJaOhO9Cqk6.jpg",  genreIDS: [35, 18], releaseDate: "2025-05-08")
     }
     
     static var mockTVwithNilPosterURL: Media {
-        Media(id: 261980, mediaType: .tv, title: "미지의 서울", overview: "얼굴 빼고 모든 게 다른 쌍둥이 자매가 인생을 맞바꾸는 거짓말로 진짜 사랑과 인생을 찾아가는 로맨틱 성장 드라마", posterPath: nil, backdropPath: nil,  genreIDS: [35, 18])
+        Media(id: 261980, mediaType: .tv, title: "미지의 서울", overview: "얼굴 빼고 모든 게 다른 쌍둥이 자매가 인생을 맞바꾸는 거짓말로 진짜 사랑과 인생을 찾아가는 로맨틱 성장 드라마", posterPath: nil, backdropPath: nil,  genreIDS: [35, 18], releaseDate: nil)
     }
 }
