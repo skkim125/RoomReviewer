888166912342755a886ef280076e8e55963f859a
skkim125 <kthanks125@gmail.com>
2025-08-19 15:04:29 +0900

    [Feat] 장르 열거형 생성 및 장르 표시 기능 구현 - TMDB 장르를 열거형으로 생성 및 문자열로의 변환 메서드 추가 - API를 통해 받아온 정수형 장르 배열을 문자열로 변환하여 장르표시에 활용

--- RoomReviewer/RoomReviewer/Network/Configuration.swift
+++ RoomReviewer/RoomReviewer/Network/Configuration.swift
@@ -23,6 +23,114 @@ enum API {
     static let authorization = "Authorization"
     static let contentType = "accept"
     static let jsonContentType = "application/json"
+
+    static func convertGenreString(_ mediaType: MediaType, array: [Int]) -> [String] {
+        let genres = array.compactMap { Genre(rawValue: $0) }
+        
+        var finalGenres: [Genre]
+
+        switch mediaType {
+        case .tv:
+            finalGenres = genres.filter { $0 != .drama }
+        case .movie, .person:
+            finalGenres = genres
+        }
+        
+        return finalGenres.map { $0.name }.sorted()
+    }
+
+    enum Genre: Int, CaseIterable {
+        // 영화 & TV 시리즈 장르
+        case action = 28
+        case adventure = 12
+        case animation = 16
+        case comedy = 35
+        case crime = 80
+        case documentary = 99
+        case drama = 18
+        case family = 10751
+        case fantasy = 14
+        case history = 36
+        case horror = 27
+        case music = 10402
+        case mystery = 9648
+        case romance = 10749
+        case scienceFiction = 878
+        case tvMovie = 10770
+        case thriller = 53
+        case war = 10752
+        case western = 37
+        
+        // TV 시리즈 장르
+        case actionAndAdventure = 10759
+        case kids = 10762
+        case news = 10763
+        case reality = 10764
+        case sciFiAndFantasy = 10765
+        case soap = 10766
+        case talk = 10767
+        case warAndPolitics = 10768
+
+        // 장르 이름
+        var name: String {
+            switch self {
+            case .action:
+                return "액션"
+            case .adventure:
+                return "모험"
+            case .animation:
+                return "애니메이션"
+            case .comedy:
+                return "코미디"
+            case .crime:
+                return "범죄"
+            case .documentary:
+                return "다큐멘터리"
+            case .drama:
+                return "드라마"
+            case .family:
+                return "가족"
+            case .fantasy:
+                return "판타지"
+            case .history:
+                return "역사"
+            case .horror:
+                return "공포"
+            case .music:
+                return "음악"
+            case .mystery:
+                return "미스터리"
+            case .romance:
+                return "로맨스"
+            case .scienceFiction:
+                return "SF"
+            case .tvMovie:
+                return "TV 영화"
+            case .thriller:
+                return "스릴러"
+            case .war:
+                return "전쟁"
+            case .western:
+                return "서부"
+            case .actionAndAdventure:
+                return "액션 & 모험"
+            case .kids:
+                return "키즈"
+            case .news:
+                return "뉴스"
+            case .reality:
+                return "리얼리티"
+            case .sciFiAndFantasy:
+                return "SF & 판타지"
+            case .soap:
+                return "연속극"
+            case .talk:
+                return "토크쇼"
+            case .warAndPolitics:
+                return "전쟁 & 정치"
+            }
+        }
+    }
 }
 
 enum HTTPMethod: String {
