e41b4a68729cc661090e76dc3c29336877e39677
skkim125 <kthanks125@gmail.com>
2025-09-01 21:50:15 +0900

    [Feat] 상세뷰의 컨텐츠 설명에 대한 더보기 버튼 추가 및 로직 구현

---
+++ RoomReviewer/RoomReviewer/Extension/String+.swift
@@ -0,0 +1,17 @@
+//
+//  String+.swift
+//  RoomReviewer
+//
+//  Created by 김상규 on 9/1/25.
+//
+
+import UIKit
+
+extension String {
+    func height(width: CGFloat, font: UIFont) -> CGFloat {
+        let constraintRect = CGSize(width: width, height: .greatestFiniteMagnitude)
+        let boundingBox = self.boundingRect(with: constraintRect, options: .usesLineFragmentOrigin, attributes: [NSAttributedString.Key.font: font], context: nil)
+        
+        return ceil(boundingBox.height)
+    }
+}
