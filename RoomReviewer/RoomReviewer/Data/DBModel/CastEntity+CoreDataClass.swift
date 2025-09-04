184cdfd2ca02b4b9cc32228e09acc6dc856c58bc
skkim125 <kthanks125@gmail.com>
2025-09-04 21:49:53 +0900

    [Feat] Crew & Cast에 대한 coredata 모델링 진행

---
+++ RoomReviewer/RoomReviewer/Data/DBModel/CastEntity+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  CastEntity+CoreDataClass.swift
+//  RoomReviewer
+//
+//  Created by 김상규 on 9/4/25.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(CastEntity)
+public class CastEntity: NSManagedObject {
+
+}
---
+++ RoomReviewer/RoomReviewer/Data/DBModel/CrewEntity+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  CrewEntity+CoreDataClass.swift
+//  RoomReviewer
+//
+//  Created by 김상규 on 9/4/25.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(CrewEntity)
+public class CrewEntity: NSManagedObject {
+
+}
