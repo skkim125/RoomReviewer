bc056ae03bc13cbc7c8af722b7b7e380e7a4f206
skkim125 <kthanks125@gmail.com>
2025-09-06 17:27:29 +0900

    [Feat] 앱 아이콘 추가 & SearchMedia의 UI 네이밍 변경 - 앱 아이콘 추가 - SearchMedia의 컬렉션뷰셀과 리액터의 네이밍을 ThreeColumnPoster로 변경

---
+++ RoomReviewer/RoomReviewer/SearchMedia/Cell/ThreeColumnPosterCollectionViewCell.swift
@@ -0,0 +1,107 @@
+//
+//  ThreeColumnMediaCollectionViewCell.swift
+//  RoomReviewer
+//
+//  Created by 김상규 on 7/12/25.
+//
+
+import UIKit
+import RxSwift
+import RxCocoa
+import ReactorKit
+import SnapKit
+import Then
+import ImageIO
+
+final class ThreeColumnPosterCollectionViewCell: UICollectionViewCell, View {
+    static let cellID = "ThreeColumnMediaCollectionViewCell"
+    var disposeBag = DisposeBag()
+    
+    private let shadowView = UIView().then {
+        $0.layer.shadowColor = UIColor.black.cgColor
+        $0.layer.shadowOpacity = 0.25
+        $0.layer.shadowOffset = CGSize(width: 0, height: 2)
+        $0.layer.shadowRadius = 4
+        $0.layer.cornerRadius = 12
+        $0.backgroundColor = .clear
+    }
+    private let posterImageView = UIImageView().then {
+        $0.layer.cornerRadius = 12
+        $0.clipsToBounds = true
+        $0.contentMode = .scaleAspectFill
+    }
+    private let activityIndicator = UIActivityIndicatorView(style: .medium)
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configureHierarchy()
+        configureLayout()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+extension ThreeColumnPosterCollectionViewCell {
+    func bind(reactor: ThreeColumnPosterCollectionViewCellReactor) {
+        reactor.state.map({ $0.isLoading })
+            .distinctUntilChanged()
+            .observe(on: MainScheduler.instance)
+            .bind(to: activityIndicator.rx.isAnimating)
+            .disposed(by: disposeBag)
+
+        reactor.state.map { $0.imageData }
+            .distinctUntilChanged()
+            .bind(with: self) { owner, image in
+                if let image = image {
+                    if image == AppImage.emptyPosterImage {
+                        owner.posterImageView.contentMode = .scaleAspectFit
+                        owner.posterImageView.backgroundColor = AppColor.appLightGray
+                        owner.posterImageView.tintColor = AppColor.appWhite
+                    }
+                    owner.posterImageView.image = image
+                } else {
+                    owner.posterImageView.backgroundColor = AppColor.appDarkGray
+                }
+            }
+            .disposed(by: disposeBag)
+
+        
+        reactor.state.map { $0.imageData }
+            .distinctUntilChanged()
+            .filter { $0 == nil }
+            .map { _ in
+                ThreeColumnPosterCollectionViewCellReactor.Action.loadImage
+            }
+            .bind(to: reactor.action)
+            .disposed(by: disposeBag)
+    }
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        reactor = nil
+        posterImageView.image = nil
+    }
+    
+    private func configureHierarchy() {
+        contentView.addSubview(shadowView)
+        shadowView.addSubview(posterImageView)
+        contentView.addSubview(activityIndicator)
+    }
+    
+    private func configureLayout() {
+        shadowView.snp.makeConstraints {
+            $0.top.horizontalEdges.equalToSuperview()
+            $0.height.equalTo(shadowView.snp.width).multipliedBy(1.5)
+        }
+
+        posterImageView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
+        
+        activityIndicator.snp.makeConstraints {
+            $0.center.equalTo(posterImageView)
+        }
+    }
+}
---
+++ RoomReviewer/RoomReviewer/SearchMedia/Cell/ThreeColumnPosterCollectionViewCellReactor.swift
@@ -0,0 +1,66 @@
+//
+//  SearchMediaCollectionViewCellReactor.swift
+//  RoomReviewer
+//
+//  Created by 김상규 on 7/13/25.
+//
+
+import UIKit
+import RxSwift
+import RxDataSources
+import ReactorKit
+
+final class ThreeColumnPosterCollectionViewCellReactor: Reactor {
+    enum Action {
+        case loadImage
+    }
+
+    enum Mutation {
+        case setLoading(Bool)
+        case setImageData(UIImage?)
+    }
+
+    struct State {
+        var mediaName: String?
+        var mediaPosterURL: String?
+        var imageData: UIImage?
+        var isLoading: Bool = false
+    }
+
+    var initialState: State
+    private let imageLoader: ImageProviding
+
+    init(media: Media, imageLoader: ImageProviding) {
+        self.initialState = State(mediaName: media.title, mediaPosterURL: media.posterPath)
+        self.imageLoader = imageLoader
+    }
+
+    func mutate(action: Action) -> Observable<Mutation> {
+        switch action {
+        case .loadImage:
+            guard let url = currentState.mediaPosterURL else {
+                return .just(.setImageData(AppImage.emptyPosterImage))
+            }
+            let imageStream = imageLoader.fetchImage(from: url)
+                .observe(on: MainScheduler.instance)
+                .map { Mutation.setImageData($0) }
+            
+            return Observable.concat([
+                .just(.setLoading(true)),
+                imageStream,
+                .just(.setLoading(false)),
+            ])
+        }
+    }
+
+    func reduce(state: State, mutation: Mutation) -> State {
+        var newState = state
+        switch mutation {
+        case .setImageData(let data):
+            newState.imageData = data
+        case .setLoading(let isLoading):
+            newState.isLoading = isLoading
+        }
+        return newState
+    }
+}
